8bc8bc10eb941ef64e3791dfd5571c6c1ae11e86
Bojan Rodic <99957259+najob-svg@users.noreply.github.com>
2025-05-09 11:09:18 +0200

    add forkObstacle unit test

---
+++ src/test/java/ch/IP12/fish/model/ForkObstacleTest.java
@@ -0,0 +1,54 @@
+package ch.IP12.fish.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.*;
+import ch.IP12.fish.model.animations.Spritesheets;
+import ch.IP12.fish.testUtils.WatchTests;
+import javafx.application.Platform;
+
+@WatchTests
+public class ForkObstacleTest {
+
+    private static World world;
+    private static Player player;
+
+    @BeforeAll
+    static void initJfxRuntime() {
+        try {
+            Platform.runLater(() -> {});
+        } catch (Exception e) {
+            Platform.startup(() -> {});
+        }
+    }
+
+    @BeforeEach
+    public void initMocks(){
+        world = mock(World.class);
+        player = mock(Player.class);
+
+        when(player.getX()).thenReturn(100.0);
+        when(player.getY()).thenReturn(100.0);
+        when(world.getRandomPlayer()).thenReturn(player);
+        when(world.currentTimeSeconds()).thenReturn(999.9);
+    }
+
+    @Test
+    public void instanciateObstacle(){
+        assertDoesNotThrow(() -> {
+            Obstacle obstacle = new Obstacle(0, 0, Spritesheets.Player.getSpriteAnimation(), world);
+            ForkObstacle forkObstacle = new ForkObstacle(obstacle);
+        });
+    }
+
+    @Test
+    public void adjustDirectionTest(){
+        Obstacle obstacle = new Obstacle(0, 0, Spritesheets.Player.getSpriteAnimation(), world);
+        ForkObstacle splitterObstacle = new ForkObstacle(obstacle);
+
+        assertDoesNotThrow(() -> {
+            splitterObstacle.adjustDirection();
+        });
+    }
+}
