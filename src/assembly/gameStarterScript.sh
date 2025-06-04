080501452d0bf25705799386ce3315534eb2096c
Klemen-Z <klemen.zmahar@students.fhnw.ch>
2025-06-04 10:50:26 +0200

    renamed window that game is in, added service that can be copied into relevant user service folder & fixed script to now run at first user login

--- src/assembly/gameStarterScript.sh
+++ src/assembly/gameStarterScript.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+lxterminal --command "echo 'Starting game'; sleep 20" &
 cd $HOME/
-sh ./deploy/Ueberduengung/start.sh ./deploy/Ueberduengung/ Ueberduengung
+sh ./deploy/Ueberduengung/start.sh ./deploy/Ueberduengung/ Ueberduengung &
+sleep 10
+wmctrl -a "Plastic Dive"
